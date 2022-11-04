a4c77aae33b312a6222598cfe8a5739049adfcba
Jeffrey Sheets <84060666+Jeffrey-Sheets@users.noreply.github.com>
2022-11-04 10:18:06 -0400

    Add files via upload

---
+++ client/Backend/Buffalo.java
@@ -0,0 +1,13 @@
+
+public class Buffalo extends Herbivore {
+	
+	public Buffalo() {
+		size = 2000;
+		hunger = 0;
+		hungerResistance = 0;
+		thirstResistance = 0;
+		animalType = "Herbivore";
+		
+	}
+
+}
---
+++ client/Backend/Carnivore.java
@@ -0,0 +1,22 @@
+
+public class Carnivore extends animal {
+	public boolean isSpotted;
+	
+	/**
+	 * This method allows the Carnivore to see in a half circle in front of them to identify any Herbivores they can hunt.
+	 */
+	public void fieldOfView() {
+		
+	}
+	
+	/**
+	 * This method occurs when a carnivore is hungry. They will initially try to sneak up on an herbivore and get as close as they can
+	 * before getting spotted.
+	 */
+	public void hunt() {
+		
+	}
+	
+	
+
+}
